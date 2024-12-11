9195d396a2e823f680ea1450d559858a82b08992
Moritz Lang <16192401+slashmo@users.noreply.github.com>
2024-12-11 21:32:51 +0100

    [CI] Run unit tests in PR workflow

--- Sources/OpenFeature/OpenFeatureFieldValue.swift
+++ Sources/OpenFeature/OpenFeatureFieldValue.swift
@@ -1,3 +1,16 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift OpenFeature open source project
+//
+// Copyright (c) 2024 the Swift OpenFeature project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 import Foundation
 
 public enum OpenFeatureFieldValue {
